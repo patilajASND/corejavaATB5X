a570ac8fc72a62664616b217ad264dbc97b90faa
patilajANSD <patilomkar1025@gmail.com>
2023-12-26 22:02:58 +0530

    fourth

--- src/LABOO1.java
+++ src/LABOO1.java
@@ -1,6 +1,11 @@
 public class LABOO1 {
-    public static void main(String [] args){
+    public static void main(String[] args) {
         System.out.println("hello world");
-    }
 
+        int a = 10;
+        int b = 20;
+        int sum = a + b;
+        System.out.println(sum);
+        System.out.println("patil");
+    }
 }
