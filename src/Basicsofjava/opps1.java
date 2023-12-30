4686452bf0d7a9b6478fb338997c4fd5fa9dd104
patilajANSD <patilomkar1025@gmail.com>
2023-12-30 11:40:19 +0530

    my commit

---
+++ src/Basicsofjava/opps1.java
@@ -0,0 +1,14 @@
+package Basicsofjava;
+
+public class opps1 {
+    public static void main(String []args){
+      int c=sum(3,4);
+
+        System.out.println(c);
+    }
+    static int sum(int a, int b){
+        return (a+b);
+    }
+
+    }
+}
