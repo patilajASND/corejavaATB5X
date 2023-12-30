e2f7d6cea0a0ca8074ed9f5f73655f957930c235
patilajANSD <patilomkar1025@gmail.com>
2023-12-30 09:59:44 +0530

    omkar

---
+++ src/Basicsofjava/switch1.java
@@ -0,0 +1,26 @@
+package Basicsofjava;
+
+import java.util.Scanner;
+
+public class switch1 {
+
+    public static void main(String[] args){
+
+        Scanner sc=new Scanner(System.in);
+       // System.out.println("Enter your day");
+//        String day=sc.nextLine();
+//        switch (day){
+//            case "monday":
+//                System.out.println("hockey");
+//                break;
+//            case "tuesday":
+//                System.out.println("Cricket");
+//                break;
+//            case "wensday":
+//                System.out.println("volley ball");
+//                break;
+//      ================= Leap Year        =======================
+
+
+    }
+}
--- src/conditions.java
+++ src/conditions.java
@@ -1,18 +1,65 @@
  import java.sql.SQLOutput;
  import java.util.*;
 public class conditions {
-    public static void main(String [] args){
-        Scanner sc=new Scanner(System.in);
-        System.out.println("Enter your name ");
-        String Name =sc.nextLine();
-        System.out.println("Enter Your age");
-        int age=sc.nextInt();
+    public static void main(String [] args) {
+        Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter your name ");
+//        String Name =sc.nextLine();
+//        System.out.println("Enter Your age");
+//        int age=sc.nextInt();
+//
+//        if (age==18){
+//            System.out.println("your are Adult");
+//        }else{
+//            System.out.println("Your are not Adult ");
+//        }
+     // System.out.println("Enter your First Number");
+//        int x = sc.nextInt();
+//        if (x % 2 == 0) {
+//            System.out.println("It is a even Number");
+//        } else {
+//            System.out.println("It is not a Even Number");
+//        }
+//            int a =sc.nextInt();
+//        System.out.println("Enter your Second  Number");
+//            int b=sc.nextInt();
+//        System.out.println("Enter your Third Number");
+//        int c=sc.nextInt();
+//        if (a==b){
+//                System.out.println("Equal");
+//            }else{
+//                System.out.println("NOt Equal");
+//            ;//            int a=sc.nexto
 
-        if (age==18){
-            System.out.println("your are Adult");
+
+           int a=50;
+           int b=20;
+           int c=40;
+
+        if (a>b && a>c){
+            System.out.println( a+  "a is Greater");
+        } else if (b>c) {
+            System.out.println( b +  "b is Greater");
         }else{
-            System.out.println("Your are not Adult ");
+            System.out.println( c +  "c is Greater");
         }
+
+//        if (a > b && b > c) {
+//            System.out.println(b + "is Middle Number");
+//        } else if (b > a && a > c) {
+//            System.out.println(a + "is Middle Number");
+//        } else {
+//            System.out.println(c + "is Middle Number");
+//        }
+
     }
 
 }
+
+
+
+
+
+
+
+
