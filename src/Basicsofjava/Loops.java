5ffc5bee016a9ab29182415fa6b879b65c2885de
patilajANSD <patilomkar1025@gmail.com>
2024-01-01 19:26:20 +0530

    commit

---
+++ src/Basicsofjava/Loops.java
@@ -0,0 +1,20 @@
+package Basicsofjava;
+
+public class Loops {
+    public static void main(String [] args){
+        for (int i=0;i<11;i++){
+            System.out.println(i);
+        }
+        int a=0;
+        while(a<11){
+            System.out.println(a);
+            a++;
+        }
+        int b=0;
+        do {
+            System.out.println(b);
+            b++;
+        }while(b<11);
+    }
+
+}
--- src/Basicsofjava/opps1.java
+++ src/Basicsofjava/opps1.java
@@ -3,12 +3,16 @@ package Basicsofjava;
 public class opps1 {
     public static void main(String []args){
       int c=sum(3,4);
+    //float d =sum1(2.4,6.7);
+      System.out.println(c);
+    }
+   public static int sum(int a, int b){
+        return a+b;
+    }
+    //public static float sum1(int pallavi, int omkar){
+    // return pallavi+ omkar;
+    
+    }
 
-        System.out.println(c);
-    }
-    static int sum(int a, int b){
-        return (a+b);
-    }
 
-    }
-}
+
