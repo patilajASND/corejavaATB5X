9c18cc9e642e4d8c19e7010a7a60ba8522f1035c
patilajANSD <patilomkar1025@gmail.com>
2024-01-02 20:05:23 +0530

    commit

---
+++ src/Examples/lab003.java
@@ -0,0 +1,23 @@
+package Examples;
+
+public class lab003 {
+    public static void main(String[] args) {
+
+        int n = 4;
+        int m = 5;
+        int j;
+        //inner loop
+        for (int i=1; i<=n;i++){
+            //outer loop
+            for(j=1;j<=5;j++){
+               if (i==1||j==1||i==n ||j==m){
+                   System.out.print("*");
+               }else{
+                   System.out.print(" ");
+               }
+            }
+            System.out.println();
+        }
+
+    }
+}
