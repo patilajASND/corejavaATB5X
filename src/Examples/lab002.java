443cf5ffc30e77ca8f9f03f7f65b9e6285e2ac92
patilajANSD <patilomkar1025@gmail.com>
2024-01-02 19:50:10 +0530

    commit

---
+++ src/Examples/lab002.java
@@ -0,0 +1,17 @@
+package Examples;
+import java.util.*;
+public class lab002 {
+    public static void main(String [] args){
+
+        int n=6;
+        int m=8;
+
+int j;
+        for (int i=1;i<=n; i++){
+            for (j=1;j<=m;j++){
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+    }
+}
