8304a427e7fe665ec94ba708417e6df01a862f60
patilajANSD <patilomkar1025@gmail.com>
2024-01-09 22:08:18 +0530

    commit

---
+++ src/oops1/Programming.java
@@ -0,0 +1,21 @@
+package oops1;
+
+public class Programming {
+
+    int version;
+    String author;
+
+    Programming() {
+        System.out.println("DC");
+    }
+        Programming(String author,int version){
+            this.author=author;
+            this.version=version;
+        }
+
+        void print1(){
+            System.out.println(this.author+ this.version);
+
+        }
+    }
+
--- src/oops1/inhertance.java
+++ src/oops1/inhertance.java
@@ -1,5 +1,15 @@
 package oops1;
 
 public class inhertance {
+   // inheritance provides a way to create a new class from exising class
+    //superclass|parentclass|base class|
+    //subclass|childclass|derivedclass
+public static void main(String[]args){
 
+    Programming p=new Programming(); //parent class
+
+    Programming p1=new Programming("peral",12);
+    p1.print1();
+
+   }
 }
