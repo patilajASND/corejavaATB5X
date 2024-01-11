5da58ba1690968614e8e8e9bc07e7fc72f12311b
patilajANSD <patilomkar1025@gmail.com>
2024-01-11 19:59:36 +0530

    commit

--- src/Basicsofjava/stringbuilder.java
+++ src/Basicsofjava/stringbuilder.java
@@ -11,5 +11,17 @@ public class stringbuilder {
         //set char at index 0
         sb.setCharAt(0,'o');
         System.out.println(sb);
+
+        //insert char at index 0
+        sb.insert(0,'s');
+        System.out.println(sb);
+
+        //insert at any index
+        sb.insert(3,'a');
+        System.out.println(sb);
+
+        //delete index at any index
+        sb.delete(2,3);
+        System.out.println(sb);
     }
 }
