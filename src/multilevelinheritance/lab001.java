b7b58c618765212611dd1c1f725b6d3227e38447
patilajANSD <patilomkar1025@gmail.com>
2024-01-15 20:02:23 +0530

    commit

--- src/multilevelinheritance/lab001.java
+++ src/multilevelinheritance/lab001.java
@@ -2,10 +2,19 @@ package multilevelinheritance;
 
 public class lab001 {
     public static void main(String[] args) {
-        grandfather g=new grandfather();
-        father f=new father();
+//        grandfather g=new grandfather();
+//        father f=new father();
+//
+//        Son s=new Son();
+//        s.bhk3();
 
-        Son s=new Son();
-        s.bhk3();
+        grandfather g=new Son();
+
+        father f =new Son();
+
+        Son S=new Son();
+
+
+
     }
 }
