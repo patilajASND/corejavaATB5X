1235a17001aadeb1e46c38914d3dd0bcc703dbcf
patilajANSD <patilomkar1025@gmail.com>
2024-01-18 22:20:47 +0530

    commit

--- src/poly/overriding/RC.java
+++ src/poly/overriding/RC.java
@@ -4,7 +4,12 @@ public class RC {
     public static void main(String[] args) {
         Dog d= new Dog();
         d.bark();
+        Honund h=new Honund();
+        h.bark();
+      Dog f=new Honund();
+      //this  is run time polymorphisam      f.bark();
+      f.bark();
+
     }
-    Honund h= new Honund();
 
 }
