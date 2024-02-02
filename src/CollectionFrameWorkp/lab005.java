a52f902bf94475627875f3dedf866af02cf37e6b
patilajANSD <patilomkar1025@gmail.com>
2024-02-02 23:28:13 +0530

    commit

---
+++ src/CollectionFrameWorkp/lab005.java
@@ -0,0 +1,31 @@
+package CollectionFrameWorkp;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class lab005 {
+
+//  How to check if an ArrayList is empty using the isEmpty() method.
+//  How to find the size of an ArrayList using the size() method.
+//  How to access the element at a particular index in an ArrayList using the get() method.
+//  How to modify the element at a particular index in an ArrayList using the set() method.
+
+    public static void main(String[] args) {
+        List<String> topProgrammingLanguages=new ArrayList<>();
+
+        System.out.println("Is the topProgrammingLanguages List Empty :"
+        + topProgrammingLanguages.isEmpty());
+        topProgrammingLanguages.add("c");
+        topProgrammingLanguages.add("java");
+        topProgrammingLanguages.add("c++");
+        topProgrammingLanguages.add("python");
+        System.out.println("Here is the Top "+ topProgrammingLanguages.size()+ "Programming Languages of World");
+
+    String bestProgrammingLang=topProgrammingLanguages.get(1);
+        System.out.println(bestProgrammingLang);
+        System.out.println( topProgrammingLanguages.get(3));
+       topProgrammingLanguages.set(3,".net");
+        System.out.println(topProgrammingLanguages);
+
+    }
+}
